00870514854e20d16535050db89b81be324e3edc
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-30 01:24:54 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,27 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-native/all',
+    'eslint:recommended', // Core ESLint recommended rules
+    'prettier', // Use Prettier for code formatting
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Enable JSX syntax
+    },
+    ecmaVersion: 2020, // Use latest ECMAScript features
+    sourceType: 'module',
+  },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Ignore unused variables starting with _
+    '@typescript-eslint/no-explicit-any': 'warn', // Warn on `any` type
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Turn off explicit return types for functions
+    'react/prop-types': 'off', // Disable prop-types as we use TypeScript
+    'react-native/no-inline-styles': 'warn', // Warn for inline styles
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the React version
+    },
   },
 };
