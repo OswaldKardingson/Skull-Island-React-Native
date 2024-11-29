ff3ddf1c28b96817fad99a5f9d791d0a6ef3ed73
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-30 00:47:13 +0200

    Delete build_ios_library.sh

--- Skull-Island-master/build_ios_library.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd cordova-plugin-litewallet/native/rust/ios
-
-cargo lipo --release
-cbindgen src/lib.rs -l c > rust.h
-
-cp ../target/universal/release/librustios.a ../../ios
-cp rust.h ../../ios/
