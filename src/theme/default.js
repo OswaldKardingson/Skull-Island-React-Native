27dadb1caeb6f8ce435a82df2506aff48d0212f7
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-30 00:29:23 +0200

    Delete default.js

--- src/theme/default.js
+++
@@ -1,6 +0,0 @@
-export const SCREEN_SIZE = {
-  height: props => props.theme.height,
-  width: props => props.theme.width,
-  topBuffer: props => props.theme.topBuffer,
-  bottomBuffer: props => props.theme.bottomBuffer,
-}
